fcacf44882f90c4a6be1c6b5bb3b2c7a6dd11ee2
Hopper262 <hopper@whpress.com>
2024-05-31 14:01:50 -0400

    version 1.1

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+Red Light version 1.1
+by Hopper
+
+The wait is over -- you no longer need to decide between playing "Red Light, Green Light" or shooting someone in the face.
+
+Add this script to your regular netgame, and you'll have a global stoplight in effect. If you move or fire during a red light, you'll be killed by the omnipotent Marathon Traffic Cop, so be sure to stop when you hear the siren.
+
+Don't host with the "netscript" gametype, just add it to your regular game of EMFH or whatever.
+
+Problems? Don't ask me, I dunno.
+
+---
+
+Version 1.1 adds a message when someone runs the light, adds a red-light sound, and adjusts some of the wait times. A version with different sounds is included for use with M1A1. Thanks to Sharkie Lino and his play-testing crew for the changes.
---
+++ Red Light M1A1.lua
@@ -0,0 +1,73 @@
+-- Red Light version 1.1
+-- (M1A1 edition)
+-- by Hopper and Sharkie Lino
+
+Triggers = { }
+
+function Triggers.init()
+  gamestate = "Red"
+  ticksleft = 0
+end
+
+function Triggers.idle()
+  -- update state machine
+  ticksleft = ticksleft - 1
+  if ticksleft < 1 then
+    if gamestate == "Red" then
+      gamestate = "Green"
+      ticksleft = rsecs(3, 30)
+      do_sound("puzzle switch")
+    elseif gamestate == "Green" then
+      gamestate = "Yellow"
+      ticksleft = 80
+      do_sound("juggernaut warning")
+    elseif gamestate == "Yellow" then
+      gamestate = "Red"
+      ticksleft = rsecs(0, 9)
+      do_sound("cant toggle switch")
+    end
+    set_color(gamestate)
+  end
+  
+  -- punish movement or firing on red state
+  if gamestate == "Red" then
+    for p in Players() do
+      if not p.dead then
+        if (not (p.internal_velocity.forward == 0)) or (not (p.internal_velocity.perpendicular == 0)) or (p.action_flags.left_trigger == true) or (p.action_flags.right_trigger == true) then
+          punish_player(p)
+        end
+      end
+    end
+  end
+end
+
+function do_sound(name)
+  local snd = Sounds[name]
+  for p in Players() do
+    p:play_sound(snd)
+  end
+end
+
+function punish_player(player)
+  player.invincibility_duration = 0
+  player.life = 0
+  player:damage(1)
+  Players.print("Hey, " .. player.name .. ". Red means stop!")
+end
+
+function set_color(state)
+  local clr = 0
+  if state == "Red" then clr = 2 end
+  if state == "Yellow" then clr = 5 end
+--  clr = OverlayColors[state].index
+  for p in Players() do
+    for i = 0,5 do
+      p.overlays[i].text = state
+      p.overlays[i]:fill_icon(clr)
+    end
+  end
+end
+
+function rsecs(min, max)
+  return math.floor((min * 30) + Game.global_random(max * 30))
+end
